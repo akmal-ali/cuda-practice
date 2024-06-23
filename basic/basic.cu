da680fe69c1730fca64bb20c6d2da30e0e6912bc
Akmal Ali <33060791+akmal-ali@users.noreply.github.com>
2024-06-23 18:17:20 +0100

    Add basic test.

--- basic/basic.cu
+++ basic/basic.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <vector>
 
 template<typename T>
 std::shared_ptr<T> CreateTensor(size_t N)
@@ -7,6 +8,24 @@ std::shared_ptr<T> CreateTensor(size_t N)
     T * data;
     cudaMalloc(&data, N*sizeof(T));
     return std::shared_ptr<T>(data, [](T* data){ cudaFree(data);});
+}
+
+template<typename T, N>
+struct CudaVector
+{
+    std::array<T, N> host;
+    void ToDevice()
+    {
+        deviceMemory = CreateTensor<T>(host.size());
+        cudaMemcpy(deviceMemory.get(), host.data(), sizeof(T)*host.size(), cudaMemcpyHostToDevice);
+    }
+
+    void ToHost()
+    {
+        cudaMemcpy(host.data(), deviceMemory.get(), sizeof(T)*host.size(), cudaMemcpyDeviceToHost);
+    }
+
+    std::shared_ptr<T> deviceMemory;
 }
 
 __global__ void VecAdd(float* x1, float* x2, float* y)
@@ -17,12 +36,28 @@ __global__ void VecAdd(float* x1, float* x2, float* y)
 
 int main()
 {
-    auto x1 = CreateTensor<float>(1024);
-    auto x2 = CreateTensor<float>(1024);
-    auto y  = CreateTensor<float>(1024);
+    CudaVector<float, 1024> x1;
+    CudaVector<float, 1024> x2;
+    CudaVector<float, 1024> y;
+
+    for(int i = 0 ; i < x1.host.size(); ++i)
+    {
+        x1.host[i] = i*1.0f;
+        x2.host[i] = 1024.0f - i*1.0f;
+        y.host[i]  = 0.0f;
+    }
+
+    x1.ToDevice();
+    x2.ToDevice();
+    y.ToDevice();
 
     VecAdd<<<1, 1024>>>(x1.get(), x2.get(), y.get());
+    y.ToHost();
 
-    
+    for(int i = 0 ; i < x1.host.size(); ++i)
+    {
+        assert(y.host[i] == x1.host[i] + x2.host[i] );
+    }
+
     return 0;
 }
